d68ce1767517ec110c23d8fb757172a031be7ac8
Mekazara <mekuchazara@gmail.com>
2022-02-23 21:22:40 +0600

    tests added

--- core/courses/urls.py
+++ core/courses/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('courses/', views.CourseList.as_view()),
-    path('courses/<int:pk>/', views.CourseDetail.as_view()),
+    path('courses/', views.CourseList.as_view(), name='list'),
+    path('courses/<int:pk>/', views.CourseDetail.as_view(), name='course_detail'),
 ]
